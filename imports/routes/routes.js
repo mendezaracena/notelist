b599b39c4854aacab4181e53f1737975db3480ff
Kevin Mendez <mendezaracena@gmail.com>
2017-08-30 14:55:20 -0700

    refactor router to allow dynamic urls

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -9,35 +9,16 @@ import Signup from '../ui/Signup';
 import Dashboard from '../ui/Dashboard';
 import NotFound from '../ui/NotFound';
 
-// Categotize pages
-// Show the rest of the pages to all users. (i.e. 404)
-const unauthenticatedPages = ['/','/signup'];
-const authenticatedPages = ['/dashboard'];
-
-const onEnterPublicPage = () => {
-  if (Meteor.userId()) {
-    browserHistory.replace('/dashboard');
-  }
-};
-
-const onEnterPrivatePage = () => {
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-};
-
 const onEnterNotePage = (nextState) => {
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  } else {
-    Session.set('selectedNoteId', nextState.params.id);
-  }
+  Session.set('selectedNoteId', nextState.params.id);
+};
+const onLeaveNotePage = () => {
+  Session.set('selectedNoteId', undefined);
 };
 
-export const onAuthChange = (isAuthenticated) => {
-  const pathname = browserHistory.getCurrentLocation().pathname;
-  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+export const onAuthChange = (isAuthenticated, currentPagePrivacy) => {
+  const isUnauthenticatedPage = currentPagePrivacy === 'unauth';
+  const isAuthenticatedPage = currentPagePrivacy === 'auth';
 
   // Logic for authenticated user; If authenticated, send to /link.
   // If not logged, redirect to /
@@ -47,16 +28,24 @@ export const onAuthChange = (isAuthenticated) => {
     browserHistory.replace('/');
   }
 };
-
+export const globalOnChange = (prevState, nextState) => {
+  globalOnEnter(nextState);
+}
+export const globalOnEnter = (nextState) => {
+  const lastRoute = nextState.routes[nextState.routes.length - 1];
+  Session.set('currentPagePrivacy', lastRoute.privacy);
+};
 // Setting routes
 export const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="/dashboard/:id" component={Dashboard} onEnter={onEnterNotePage}/>
+    <Route onEnter={globalOnEnter} onChange={globalOnChange}>
+      <Route path="/" component={Login} privacy="unauth"/>
+      <Route path="/signup" component={Signup} privacy="unauth"/>
+      <Route path="/dashboard" component={Dashboard} privacy="auth"/>
+      <Route path="/dashboard/:id" component={Dashboard} privacy="auth" onEnter={onEnterNotePage} onLeave={onLeaveNotePage}/>
 
-    {/* Catch all * pages */}
-    <Route path="*" component={NotFound}/>
+      {/* Catch all * pages */}
+      <Route path="*" component={NotFound}/>
+    </Route>
   </Router>
 );
