c3fcdcb19d4e29f657b13240b1fd72a6875e8c83
Kevin Mendez <mendezaracena@gmail.com>
2017-08-31 22:02:46 -0700

    Style note list

--- imports/ui/NoteListEmptyItem.js
+++ imports/ui/NoteListEmptyItem.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const NoteListEmptyItem = () => {
   return (
-    <div>
-      <h5>You have not notes</h5>
-    </div>
+    <p className="empty-item">Create a note to get started.</p>
   );
 }
 
